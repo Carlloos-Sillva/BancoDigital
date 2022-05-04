1e707d19daf2fff55cbc9935fd2d639de2df64b8
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2022-05-03 21:32:50 -0300

    versão 01.01
    Novo ajuste

--- app/src/main/java/com/example/bancodigital/BaseDados.java
+++ app/src/main/java/com/example/bancodigital/BaseDados.java
@@ -1,4 +1,29 @@
 package com.example.bancodigital;
 
-public class BaseDados {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {ContaCorrente.class},version = 1,exportSchema = false)
+public abstract class BaseDados extends RoomDatabase {
+    // CRIANDO A INSTANCIA
+    private static BaseDados database;
+    // CRIANDO O METODO DE ACESSO A CLASSE DAO
+    public abstract DAO getDAO();
+    // MODELO Singleton
+    public static BaseDados getInstance(Context context){
+        if (database==null){
+            synchronized (BaseDados.class){
+                if(database==null){
+                    //CRIANDO O BANCO DE DADOS
+                    database= Room.databaseBuilder(context.getApplicationContext(),
+                            BaseDados.class,"BancoCC").allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return database;
+    }
 }
--- app/src/main/java/com/example/bancodigital/DAO.java
+++ app/src/main/java/com/example/bancodigital/DAO.java
@@ -1,4 +1,28 @@
 package com.example.bancodigital;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface DAO {
+    @Insert
+    void InsereConta(ContaCorrente conta);
+    @Delete
+    void DeletarConta(ContaCorrente conta);
+    @Delete
+    void resetListaContas(List<ContaCorrente>conta);
+
+    @Query("SELECT * FROM ContaCorrente")
+    List<ContaCorrente> getAll();
+
+    @Query("UPDATE ContaCorrente SET Nome = :Snome WHERE id = :Sid")
+    void update(int Sid, String Snome);
+
+    @Query("Select * FROM ContaCorrente WHERE cpf=(:cpfcc) and senha=(:senhacc)")
+    ContaCorrente Logar(String cpfcc, String senhacc);
+
 }
